790c7356e568d5783f48575a6ae81d079f676c1f
Sébastien Duruz <sebastienduruz@protonmail.com>
2025-05-05 09:23:23 +0200

    docs: Update class diagram with new Tag entity

--- src/main/java/ch/etmles/payroll/LoadDatabase.java
+++ src/main/java/ch/etmles/payroll/LoadDatabase.java
@@ -34,6 +34,7 @@ public class LoadDatabase {
             log.info("Preloading " + categoryRepository.save(childCategory));
             log.info("Preloading " + lotRepository.save(new Lot("Lot Test 1", "Test description 1", "https://picsum.photos/id/237/600/400", new BigDecimal("19.95"), LocalDateTime.now().toString(), (LocalDateTime.now()).plusDays(7).toString(), parentCategory, LotStatus.ACTIVATED,tags)));
             log.info("Preloading " + lotRepository.save(new Lot("Lot Test 2", "Test description 2", "https://picsum.photos/id/238/600/400", new BigDecimal("49.95"), LocalDateTime.now().toString(), (LocalDateTime.now()).plusDays(7).toString(), childCategory, LotStatus.ACTIVATED,tags)));
+            log.info("Swagger : http://localhost:8080/swagger-ui/index.html");
         };
     }
 }
