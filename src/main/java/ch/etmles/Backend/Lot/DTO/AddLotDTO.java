16f26a8c25c1d60ba88e3b49e25d225a30b0ea40
Sébastien Duruz <sebastienduruz@protonmail.com>
2025-05-25 15:30:28 +0200

    Add new DTO for POST route

---
+++ src/main/java/ch/etmles/Backend/Lot/DTO/AddLotDTO.java
@@ -0,0 +1,81 @@
+package ch.etmles.Backend.Lot.DTO;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public class AddLotDTO {
+    public String name;
+    public String description;
+    public String pictureUrl;
+    public LocalDateTime endDate;
+    public BigDecimal startPrice;
+    public UUID categoryId;
+    public String status;
+    public List<String> tags;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public BigDecimal getStartPrice() {
+        return startPrice;
+    }
+
+    public void setStartPrice(BigDecimal startPrice) {
+        this.startPrice = startPrice;
+    }
+
+    public UUID getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(UUID categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+}
