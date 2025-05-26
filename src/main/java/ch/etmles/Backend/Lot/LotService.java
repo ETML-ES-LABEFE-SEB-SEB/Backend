30db79533b28bc408bb5dba89ce8c146738534d5
Sébastien Duruz <sebastienduruz@protonmail.com>
2025-05-26 08:52:37 +0200

    feat: Add a method to close a lot

--- src/main/java/ch/etmles/Backend/Lot/LotService.java
+++ src/main/java/ch/etmles/Backend/Lot/LotService.java
@@ -1,15 +1,18 @@
 package ch.etmles.Backend.Lot;
 
 import ch.etmles.Backend.Bid.Bid;
+import ch.etmles.Backend.Bid.BidService;
 import ch.etmles.Backend.Bid.DTO.BidDTO;
 import ch.etmles.Backend.Bid.BidRepository;
 import ch.etmles.Backend.Lot.Exceptions.LotNotFoundException;
 import ch.etmles.Backend.Lot.Exceptions.LotNotOpenedException;
 import ch.etmles.Backend.LotCategory.CategoryRepository;
+import ch.etmles.Backend.Member.Member;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -18,11 +21,13 @@ public class LotService {
     private final BidRepository bidRepository;
     private final LotRepository lotRepository;
     private final CategoryRepository categoryRepository;
+    private final BidService bidService;
 
-    public LotService(BidRepository bidRepository, LotRepository lotRepository, CategoryRepository categoryRepository) {
+    public LotService(BidRepository bidRepository, LotRepository lotRepository, CategoryRepository categoryRepository, BidService bidService) {
         this.bidRepository = bidRepository;
         this.lotRepository = lotRepository;
         this.categoryRepository = categoryRepository;
+        this.bidService = bidService;
     }
 
     public Lot getOpenLotById(UUID lotId)
@@ -44,5 +49,18 @@ public class LotService {
         for(Bid bid : bids)
             bidDTOs.add(BidDTO.toDto(bid));
         return bidDTOs;
+    }
+
+    public boolean closeLot(UUID lotId)
+    {
+        Member highestBidder = bidService.getHigherBidder(lotId);
+
+        Optional<Lot> lot = lotRepository.findById(lotId);
+        if(lot.isPresent()){
+            // Remove the reserved amount from the bidder wallet
+            highestBidder.setReservedWallet(highestBidder.getReservedWallet().subtract(lot.get().getCurrentPrice()));
+        }
+
+        return false;
     }
 }
