4f685a56b4b0e34339237d2326a9d5a07897145d
Sébastien Duruz <sebastienduruz@protonmail.com>
2025-06-15 20:34:45 +0200

    feat: Load swagger only with dev profile

--- src/main/java/ch/etmles/Backend/Data/Config/SwaggerConfig.java
+++ src/main/java/ch/etmles/Backend/Data/Config/SwaggerConfig.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class SwaggerConfig {
 
     @Bean
